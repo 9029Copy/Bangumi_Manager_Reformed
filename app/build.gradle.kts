21760fa635b966b1b3860fd460b7052f5c03ca04
KHRawa <tang187402824@163.com>
2026-08-06 17:22:31 +0800

    20260806: 更新版本号为"0.1.0-alpha01"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.copy9029.bangumimanagerreformed"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
